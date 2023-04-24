7012c60d008c138d2830eedd4b7eb607a96485bc
iusto <iusto@naver.com>
2023-04-24 13:36:15 +0900

    first commit

---
+++ src/main/java/JSF_homepage/JsfHomepageApplication.java
@@ -0,0 +1,13 @@
+package JSF_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfHomepageApplication.class, args);
+	}
+
+}
